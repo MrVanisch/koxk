3b4bb1b69d6e7fc026a942e7e3968643610f3d57
MrVanisch <80252051+MrVanisch@users.noreply.github.com>
2023-03-15 14:23:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import webbrowser
 import os
 import datetime
 
+cookies = ""
 nudes = ""
 token = ""
 
@@ -24,14 +25,12 @@ class Clicker:
             return 0
 
         except:
+            print("Nie znaleziono elementu.")
             return 1
 
-def wiadomosc_dioscrd(idkanalu):
-    time.sleep(10)
+def wiadomosc_dioscrd(idkanalu,cookies):
     global nudes
     global token
-    start = Clicker('images\kod.png', speed=.8)
-    confirm = Clicker('images\kp.png', speed=.8)
     while True: 
         headers1 = {
             'authorization': token
@@ -53,23 +52,54 @@ def wiadomosc_dioscrd(idkanalu):
         if(nudes!=kod and kod != '<@&862297210828881930>' and kod !=''):
             x = datetime.datetime.now()
             print("Kod:", kod, x.strftime("%X %x"))
-            webbrowser.open('https://key-drop.com/pl/')
-            while start.nav_to_image() == 1:
-                time.sleep(2)
-            while confirm.nav_to_image() == 1:
-                time.sleep(2)           
-            time.sleep(2)
-            pyautogui.write(kod)
-            time.sleep(2)
-            pyautogui.press('enter')
-            time.sleep(2)
-            pyautogui.hotkey('ctrl', 'w')
-            time.sleep(2)
+            url = "https://key-drop.com/pl/Api/activation_code"
+            payload = json.dumps({
+            "promoCode": kod,
+            "recaptcha": None
+            })
+            headers = {
+             'sec-ch-ua': '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+            'content-type': 'application/json',
+            'x-requested-with': 'XMLHttpRequest',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+            'sec-ch-ua-platform': '"Windows"',
+            'Accept': '*/*',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Dest': 'empty',
+            'host': 'key-drop.com',
+            'Cookie': cookies
+            }
+            response = requests.request("POST", url, headers=headers, data=payload)
+            kox = json.loads(response.text)
+            print("Rezultat:",kox['status'])
             nudes = kod
-        
+        time.sleep(10)
+
+def cookieszmiana():
+    global cookies
+    start = Clicker('start.png', speed=.8)
+    potiwerdzenie = Clicker('potiwerdzenie.png', speed=.8)
+    time.sleep(5)
+    if(os.path.isfile('cookies.txt') == True):
+        os.remove('cookies.txt')
+    webbrowser.open('https://key-drop.com/pl/')
+    time.sleep(5)
+    while  start.nav_to_image() ==1:
+        time.sleep(2)
+    while potiwerdzenie.nav_to_image() ==1:
+        time.sleep(2)
+    time.sleep(2)
+    pyautogui.hotkey('ctrl', 'w')
+    file = open("cookies.txt")
+    cookies = file.read()
+    file.close()
+
+
 def get_token():
     global token
-    if(os.path.isfile('discord_token.txt') == True):
+    if(os.path.isfile('cookies.txt') == True):
         file = open("discord_token.txt")
         token = file.read()
         file.close()
@@ -77,8 +107,10 @@ def get_token():
         print("Nie ma pliku discord_token.txt lub wystompił problem z jego odczytaniem")
 
 if __name__ == '__main__':
+    cookieszmiana()
+    time.sleep(2)
     get_token()
     time.sleep(2)
-    wiadomosc_dioscrd('868574854536888401')
+    wiadomosc_dioscrd('868574854536888401',cookies)
     
 
